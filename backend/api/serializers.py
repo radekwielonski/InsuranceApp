2f7a2fa892002955a237aa122cb06cd65b13fa52
R4dziu <radek.wielonski@gmail.com>
2020-02-12 16:48:57 +0100

    add columns genereation func

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,13 +3,38 @@ from django.contrib.auth.models import User
 from api import models
 
 
+class SerializerMethods():
+    @staticmethod
+    def generate_columns_from_model(model):
+        list_of_attributes = []
+
+        for f in model._meta.get_fields():
+            list_of_attributes.append(f.name)
+
+        return list_of_attributes[1:]
+
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'groups')
 
 
+class GenderSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Gender
+        fields = ('url', 'gender')
+
+
 class ClientSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.Client
-        fields = ('url', 'name', 'adress', 'company')
+        generated_fields = SerializerMethods.generate_columns_from_model(model)
+        fields = ('url', *generated_fields)
+
+
+class IndividualClientDetailsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.IndividualClientDetails
+        generated_fields = SerializerMethods.generate_columns_from_model(model)
+        fields = ('url', *generated_fields)
